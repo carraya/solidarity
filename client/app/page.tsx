fdd6ffa56361b5c83bbd29146ce1ddab71ab286a
Christopher Arraya <arrayacj@gmail.com>
2023-04-03 14:18:24 -0400

    feat(client): add storybook + shadcn/ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,20 @@
+import { Inter as FontSans } from "next/font/google";
+
+import { Hero } from "@/components/Hero";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
 export default function Home() {
   return (
-    <main>
-      <h1>Solidarity Home</h1>
+    <main className={fontSans.className}>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Solidarity Home
+      </h1>
+      <Hero />
     </main>
   );
 }
