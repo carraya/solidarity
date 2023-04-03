c1dacc124427e3f18e1462337afc4c3b541ee461
Christopher Arraya <arrayacj@gmail.com>
2023-04-03 14:18:24 -0400

    feat(client): add storybook + shadcn/ui

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    fontLoaders: [
+      {
+        loader: "@next/font/google",
+        options: { subsets: ["latin"] },
+      },
+    ],
   },
 };
 
